2c6f774ea0be01ef0f4363ba16842d58ebcc1ddf
hezou <hezoubeke18@gmail.com>
2024-05-11 22:20:42 +0000

    packages creation

---
+++ ppoo_hbeke/src/main/java/com/mycompany/ppoo_hbeke/Ppoo_hbeke.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ppoo_hbeke;
+
+/**
+ *
+ * @author BEKE hezou
+ */
+public class Ppoo_hbeke {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
